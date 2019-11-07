fc060c9124fa2c923d71e6346fd0a06a4c1079df
Amber-Pittman <amber@amberpittman.com>
2019-11-06 21:21:30 -0500

    Completed Delete Functionality

--- src/components/Users.js
+++ src/components/Users.js
@@ -15,7 +15,32 @@ function Users(props) {
 			.catch(error => {
 				console.log(error)
 			})
-	}, [])
+    }, [])
+    
+    const handleDelete = (event, id) => {
+        event.preventDefault()
+
+        if (window.confirm("Are you sure you want to delete this user?")) {
+
+        // get user object in case we need to restore it
+        const user = users.find(user => user.id === id)
+
+        // optimistic update
+        setUsers(users.filter(user => user.id !== id))
+
+        api().delete(`/users/${id}`)
+            .then(result => {
+                console.log("User was deleted")
+                //setUsers(users.filter(user => user.id !== id)) --- moved up in case network is taking too long
+            })
+            .catch(error => {
+                console.log(error)
+
+                // put user back if the request wasn't successful
+                setUsers([...users, user])
+            })
+    }
+}
 
 	return (
 		<>
@@ -23,7 +48,8 @@ function Users(props) {
             {users.map(user => (
                 <div key={user.id} className="account">
                     <Link className="account-update" to={`/users/${user.id}`}>Edit</Link>
-                    <button className="account-delete" onClick={}>Delete</button>
+                    <button className="account-delete" onClick={(event) => handleDelete(event, user.id)}>Delete</button> 
+                                                        {/* using a callback here gets both params */}
                     <div className="account-row">Name: {user.name}</div>
                     <div className="account-row">Email: {user.email}</div>
                 </div>
